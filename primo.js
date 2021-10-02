407eaf9456f61b16dcd3950afbf3f2b89a7eb870
Pedro Lima <89553932+PedroJardel@users.noreply.github.com>
2021-10-02 09:09:03 -0300

    Update primo.js
    02/10/2021 - Alterado o método de verificaPrimo, onde ajustes foram feitos para simplificar o método,  e acrescentado uma nova forma de fazer a validação. Uma por DO WHILE como anteriormente feito e outra por FOR.

--- primo.js
+++ primo.js
@@ -2,28 +2,63 @@ var prompt = require("prompt-sync")()
 
 // @author Pedro Jardel Lima
 
-//Método que consiste em digitar um número e verificar se ele é primo ou não
+/*
+Método que consiste em digitar um número e verificar
+ se ele é primo ou não
+*/
+console.log("Digite um número, ou digite '0 ou 1' para finalizar ")
 
-console.log("Digite um número, ou digite '0' para finalizar ")
-
-var num = Number(prompt("Número: "))
-console.log("----------------------------------------------------------------")
 var i = 1
+var contador = 1 
+var num = Number(prompt("Número: "))
+console.log("-".repeat(40))
+
 do {
-    if (num == 0 || num == 1) {
-        console.log(`Os Números 0 e 1 não se aplicam à essa regra, por tanto não são primos,\n por que são divisores comuns de todos os números`)
+    if(num == 0 || num == 1){
         break
     }
+    if (num % i == 0) {
+        contador++
+    }
     i++
-    if (num == i) {
-        if (num % i === 0) {
-            console.log(`O número ${num} é primo`)
-            break
-        }
-    } else {
-        if (num % i === 0) {
-            console.log(`O número ${num} não é primo`)
-            break
+} while (i <= num / 2)
+
+if (contador == 1) {
+    console.log(`O número ${num} é primo`)
+    console.log("=".repeat(40))
+    console.log("Obrigado por utilizar nosso programa")
+    console.log("=".repeat(40))
+} else {
+    console.log(`O número ${num} não é primo`)
+    console.log("=".repeat(40))
+    console.log("Obrigado por utilizar nosso programa")
+    console.log("=".repeat(40))
+}
+
+// Outra forma de se fazer essa validação
+
+var num = Number(prompt("Número: "))
+var contador = 0
+if (num != 0 && num != 1) {
+    for (var i = 1; i <= num / 2; i++) {
+        if (num % i == 0) {
+            contador++
         }
     }
-} while (i <= num)
+    if ( contador == 1) {
+       console.log(`O número ${num} é primo`)
+       console.log("=".repeat(40))
+       console.log("Obrigado por utilizar nosso programa")
+       console.log("=".repeat(40))
+    } else {
+       console.log(`O número ${num} não é primo`)
+       console.log("=".repeat(40))
+       console.log("Obrigado por utilizar nosso programa")
+       console.log("=".repeat(40))
+    }
+} else {
+    console.log("=".repeat(40))
+    console.log("Obrigado por utilizar nosso programa")
+    console.log("=".repeat(40))
+}
+
